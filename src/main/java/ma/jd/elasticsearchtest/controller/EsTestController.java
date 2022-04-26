3cbd99c5c072cf0252f7ce8b2280f3ee389ccc16
Tianshu Liu <liutianshu_8827@163.com>
2022-04-26 13:37:43 +0800

    init

---
+++ src/main/java/ma/jd/elasticsearchtest/controller/EsTestController.java
@@ -0,0 +1,37 @@
+package ma.jd.elasticsearchtest.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+public class EsTestController {
+    private final RestHighLevelClient restHighLevelClient;
+
+    private final ElasticsearchRestTemplate elasticsearchRestTemplate;
+
+    @GetMapping("/qq")
+    public void qq() throws IOException {
+        SearchRequest searchRequest = new SearchRequest();
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchAllQuery());
+        searchRequest.source(searchSourceBuilder);
+        SearchResponse search = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
+        SearchHit[] hits = search.getHits().getHits();
+        for (int i = 0; i < hits.length; i++) {
+            System.out.println(hits[i]);
+        }
+    }
+}
