e8e6f41d91f448ca8bb741298e05c067d1441746
Tianshu Liu <liutianshu_8827@163.com>
2022-04-28 09:35:06 +0800

    4.28

--- src/main/java/ma/jd/elasticsearchtest/controller/EsTestController.java
+++ src/main/java/ma/jd/elasticsearchtest/controller/EsTestController.java
@@ -1,6 +1,7 @@
 package ma.jd.elasticsearchtest.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
@@ -9,7 +10,6 @@ import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,10 +17,9 @@ import java.io.IOException;
 
 @RestController
 @RequiredArgsConstructor(onConstructor = @__(@Autowired))
+@Slf4j
 public class EsTestController {
     private final RestHighLevelClient restHighLevelClient;
-
-    private final ElasticsearchRestTemplate elasticsearchRestTemplate;
 
     @GetMapping("/qq")
     public void qq() throws IOException {
@@ -31,7 +30,7 @@ public class EsTestController {
         SearchResponse search = restHighLevelClient.search(searchRequest, RequestOptions.DEFAULT);
         SearchHit[] hits = search.getHits().getHits();
         for (int i = 0; i < hits.length; i++) {
-            System.out.println(hits[i]);
+            log.info("第i个：{}", hits[i]);
         }
     }
 }
